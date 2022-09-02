9413b223e7dce79ee0091e4d83f1875f44a99dd4
Sarrthi Jasrotia <sarrthijasrotia05@gmail.com>
2022-09-01 23:43:04 -0500

    fixed the formating  and finished the index,head

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
-
+require("./models/pokemon.js")
+const methodOverride = require("method-override")
 const pokemon = require("./models/pokemon.js")
 
 //Middleware
-app.use(express.urlencoded({ extended: false })); 
+app.use(express.urlencoded({ extended: false }));
 
-
+app.use(methodOverride("_method"))
 
 //INDEX
 app.get("/pokemon", (req, res) => {
@@ -16,19 +17,76 @@ app.get("/pokemon", (req, res) => {
 })
 
 //NEW
+app.get("/pokemon/new", (req, res) => {
+    res.render("new.ejs")
+})
 
 //DELETE
+app.delete("/pokemon/:id", (req, res) => {
+    pokemon.splice(req.params.id, 1)
+    res.redirect("/pokemon")
+})
 
 //UPDATE
+app.put("/pokemon/:id", (req, res) => {
+    const index = req.params.id
+    const pokemonD = req.body
+    const stats = {}
+    const type = []
+
+    pokemon[index].name = pokemonD.name
+    pokemon[index].img = pokemonD.img
+
+    type.push(pokemonD.type)
+
+    stats.hp = pokemonD.hp
+    stats.attack = pokemonD.attack
+    stats.defense = pokemonD.defense
+    stats.speed = pokemonD.speed
+
+    pokemon[index].stats = stats
+    pokemon[index].type = type
+
+
+    res.redirect("/pokemon")
+})
 
 //CREATE
+app.post("/pokemon", (req, res) => {
+    //console.log(req.body)
+    const pokemonBody = req.body
+    const newPoke = {}
+    const stats = {}
+    const type = []
 
+    newPoke.name = pokemonBody.name
+    newPoke.img = pokemonBody.img
+    newPoke.stats = stats
+    newPoke.type = type
+
+    type.push(pokemonBody.type)
+
+    stats.hp = pokemonBody.hp
+    stats.attack = pokemonBody.attack
+    stats.defense = pokemonBody.defense
+    stats.speed = pokemonBody.speed
+
+
+
+    pokemon.unshift(newPoke)
+    res.redirect("/pokemon")
+})
 //EDIT
-
+app.get("/pokemon/:id/edit", (req, res) => {
+    res.render("edit.ejs", {
+        idPokemon: pokemon[req.params.id],
+        index: req.params.id
+    })
+})
 //SHOW
 app.get("/pokemon/:id", (req, res) => {
     res.render("show.ejs", {
-       idPokemon: pokemon[req.params.id]
+        idPokemon: pokemon[req.params.id]
     })
 })
 //listening
